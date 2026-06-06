5332f821cb6d8cc2d46d3321d269412f42de64be
Avafe <65048459+ImAvafe@users.noreply.github.com>
2026-06-06 13:08:47 +0800

    Handle CLI arguments for a sample command

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -1,5 +1,32 @@
-import 'package:cli/cli.dart' as cli;
+import "dart:io";
+
+const version = "0.1.0";
+
+void printHelp() {
+  print("Valid commands:\nhelp\nversion\nsearch <TITLE>");
+}
+
+void searchWikipedia(List<String>? arguments) {
+  final String searchQuery;
+
+  if (arguments == null || arguments.isEmpty) {
+    print("Please provide a search query:");
+    searchQuery = stdin.readLineSync() ?? '';
+  } else {
+    searchQuery = arguments.join(' ');
+  }
+
+  print('Search Wikipedia: $searchQuery');
+}
 
 void main(List<String> arguments) {
-  print('Hello world: ${cli.calculate()}!');
+  if (arguments.isEmpty || arguments[0] == "help") {
+    printHelp();
+  } else if (arguments[0] == "version") {
+    print('CLI version is $version');
+  } else if (arguments[0] == "search") {
+    searchWikipedia(arguments.length > 1 ? arguments.sublist(1) : null);
+  } else {
+    printHelp();
+  }
 }
